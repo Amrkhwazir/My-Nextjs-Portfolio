3e9ce2614156b06eb8a3d89c8e3962df096808a1
Amrkhwazir <amrkh5678@gmail.com>
2024-02-19 09:36:00 +0500

    navbar completed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,74 @@
-import React from 'react'
+"use client"
 
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { NavLinks } from './navLinks'
+
+
+const links = [
+  {url: "/", title: "Home"},
+  {url: "/about", title: "About"},
+  {url: "/portfolio", title: "Portfolio"},
+  {url: "/contact", title: "Contact"},
+]
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
   return (
-    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
-      <div>1</div>
-      <div>2</div>
+    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-lg'>
+      {/* LINKS */}
+      <div className='hidden md:flex gap-4 w-1/3'>
+      {
+        links.map((link)=> (
+         <NavLinks link={link} key={link.title} />
+        ))
+      }
+      </div>
+      {/* LOGO */}
+      <div className='md:hidden lg:flex xl:w-1/3 xl:justify-center'>
+      <Link href="/" className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'>
+        <span className='text-white mr-1'>lama</span>
+        <span className='w-12 h-8 rounded bg-white text-black flex items-center justify-center'>.dev</span>
+      </Link>
+      </div>
+      {/* SOCIALS */}
+      <div className='hidden md:flex gap-4 w-1/3'>
+        <Link href="#">
+      <Image src="/github.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+      <Image src="/facebook.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+      <Image src="/pinterest.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+      <Image src="/instagram.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+      <Image src="/linkedin.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
+      {/* MENU */}
+      <div className='md:hidden'>
+        
+        <button  className='w-10 h-8 flex flex-col justify-between z-50 relative' onClick={() => setOpen((prev) => !prev)}>
+          <div className=' w-10 h-1 bg-black rounded'></div>
+          <div className=' w-10 h-1 bg-black rounded'></div>
+          <div className=' w-10 h-1 bg-black rounded'></div>
+        </button>
+        {/* MENU LIST */}
+        { open &&
+
+          <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
+      {links.map((link, i)=> {
+        return  <Link href={link.url} key={i}>
+          {link.title}
+          </Link>
+      })}
+        </div>
+    }
+      </div>
     </div>
   )
 }
