dc97d36923fcfabc9406a5c517534f45650a6b09
Amrkhwazir <amrkh5678@gmail.com>
2024-04-14 18:29:37 +0500

    about page completed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -14,6 +14,9 @@ const AboutPage = () => {
   const skillRef = useRef();
   const isSkillRefInView = useInView(skillRef)
 
+  const experienceRef = useRef();
+  const isExperienceRefInView = useInView(experienceRef)
+
   return (
     <motion.div className="h-full text-slate-200" initial={{y: "-200vh"}} animate={{y: "0%"}} transition={{duration: 1.2}}>
       {/* CONTAINER */}
@@ -41,7 +44,7 @@ const AboutPage = () => {
         <Image src="/signature.png" alt="my signature" width={300} height={10} />
         </div>
          {/* BIOGRAPHY SVG  */}
-         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#e2e8f0" width={50} height={50}><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 6V14" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M15 11L12 14L9 11" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+         <motion.svg initial={{opacity: 0.2, y:0}} animate={{opacity: 1, y: "10px"}} transition={{repeat: Infinity, duration: 3, ease: "easeInOut"}} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#e2e8f0" width={50} height={50}><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 6V14" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M15 11L12 14L9 11" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></motion.svg >
         </div>
 
         {/* SKILLS CONTAINER */}
@@ -50,13 +53,12 @@ const AboutPage = () => {
         <motion.h1
         initial={{x: "-300px"}}
         animate={isSkillRefInView ? {x: 0} : {}}
-        transition={{delay: 0.6, ease: "easeIn"}}
+        transition={{delay: 0.4}}
         className='font-bold text-2xl bg-gradient-to-t from-sky-400 to-indigo-200 bg-clip-text text-transparent'>SKILLS</motion.h1>
         {/* SKILLS LIST  */}
         <motion.div 
          initial={{x: "-800px"}}
          animate={isSkillRefInView ? {x: 0} : {}}
-         transition={{delay: 0.6, ease: "easeIn"}}
         className='flex gap-4 flex-wrap'>
           <div className="rounded p-2 text-sm cursor-pointer bg-slate-200 text-black hover:bg-gradient-to-r from-sky-500 to-indigo-500 hover:text-white">JavaScript</div>
           <div className="rounded p-2 text-sm cursor-pointer bg-slate-200 text-black hover:bg-gradient-to-r from-sky-500 to-indigo-500 hover:text-white">TypeScript</div>
@@ -74,14 +76,21 @@ const AboutPage = () => {
           <div className="rounded p-2 text-sm cursor-pointer bg-slate-200 text-black hover:bg-gradient-to-r from-sky-500 to-indigo-500 hover:text-white">React Native</div>
         </motion.div>
         {/* SKILLS SVG  */}
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#f1f5f9" width={50} height={50}><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 6V14" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M15 11L12 14L9 11" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+        <motion.svg initial={{opacity: 0.2, y:0}} animate={{opacity: 1, y: "10px"}} transition={{repeat: Infinity, duration: 3, ease: "easeInOut"}} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#f1f5f9" width={50} height={50}><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 6V14" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M15 11L12 14L9 11" stroke="#f1f5f9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></motion.svg>
         </div>
         {/* EXPERIENCE CONTAINER */}
-         <div className='flex flex-col gap-12 justify-center pb-48'>
+         <div className='flex flex-col gap-12 justify-center pb-48' ref={experienceRef}>
         {/* EXPERIENCE TITLE  */}
-        <h1 className='font-bold text-2xl bg-gradient-to-t from-sky-400 to-indigo-200 bg-clip-text text-transparent'>EXPERIENCE</h1>
+        <motion.h1  
+        initial={{x: "-300px"}}
+        animate={isExperienceRefInView ? {x: 0} : {}}
+        transition={{delay: 0.4}} 
+        className='font-bold text-2xl bg-gradient-to-t from-sky-400 to-indigo-200 bg-clip-text text-transparent'>EXPERIENCE</motion.h1>
         {/* EXPERIENCE LIST  */}
-        <div className="">
+        <motion.div 
+         initial={{x: "-700px"}}
+         animate={isExperienceRefInView ? {x: 0} : {}}
+        className="">
         {/* EXPERIENCE LIST ITEM  */}
         <div className="flex justify-between h-48">
         {/* LEFT  */}
@@ -155,7 +164,7 @@ const AboutPage = () => {
         <div className="w-1/3"></div>
         </div>
          
-        </div>
+        </motion.div>
         </div>
       </div>
         {/* SVG CONTAINER */}
